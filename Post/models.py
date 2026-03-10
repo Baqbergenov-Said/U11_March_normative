19713e08a8a014c83c34eabeea5bc3cf43c0b459
Baqbergenov-Said <baqbergenovsaid@gmail.com>
2026-03-10 16:01:23 +0500

    Post CRUD completed

--- Post/models.py
+++ Post/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    image = models.ImageField(upload_to='posts/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        db_table = 'posts'
+
+    def __str__(self):
+        return self.title
