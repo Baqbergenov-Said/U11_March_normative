c8bcbc96b583e1d0fc657cfd8c2f748e9bb44c5f
Baqbergenov-Said <baqbergenovsaid@gmail.com>
2026-03-10 17:31:49 +0500

    9-normativ: Image upload configured

--- config/settings.py
+++ config/settings.py
@@ -2,15 +2,14 @@ from pathlib import Path
 import environ
 import os
 
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env(DEBUG=(bool, True))
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
