3bda42105b2c9ff5c40066ca11bc589b9a2daff2
Baqbergenov-Said <baqbergenovsaid@gmail.com>
2026-03-10 16:29:11 +0500

    Accounts register, login, logout added

--- config/settings.py
+++ config/settings.py
@@ -123,3 +123,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/posts/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
